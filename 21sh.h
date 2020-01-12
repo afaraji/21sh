9489dc861b24ee0409164744a6c717c7263afffe
afaraji <afaraji@afarajis-iMac.local>
2020-01-12 21:12:26 +0100

    added header file

---
+++ 21sh.h
@@ -0,0 +1,11 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <pwd.h>
+# include <grp.h>
+# include <time.h>
+
+#endif
