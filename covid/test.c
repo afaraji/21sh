bd62b11f1b9306f975ac939aaf2b5fdc0754725d
afaraji <afaraji@student.1337.ma>
2020-11-01 15:57:14 +0100

    norming and freeing tilde_sub, removed free(line->str) from term_free was causing double free error

--- covid/test.c
+++ covid/test.c
@@ -99,3 +99,99 @@ int main(int ac, char **av)
 	fd = open("fofo", O_CREAT | O_TRUNC | O_WRONLY, 0644);
 	return (0);
 }
+
+
+int		tilde_sub(t_list_token **cmd_token)// need to be normed and leak free
+{// neeed to add ass_word
+	char 			*tmp = NULL;
+	char			*str;
+	char 			*tilde_prefix;
+	char 			*rest;
+	t_list_token	*node;
+	struct passwd	*pw;
+	int				i;
+
+	node = *cmd_token;
+	while (node)
+	{
+		if (node->type != WORD || (node->next && (node->next->type == QUOTE || node->next->type == DQUOTE)))
+		{
+			node = node->next;
+			continue;
+		}
+		if ((i = is_assword(node->data)))
+			str = ft_strdup(&(node->data[i]));
+		else
+			str = ft_strdup(node->data);
+		if (str && str[0] == '~')
+		{
+			i = 1;
+			while (str[i] && str[i] != '/')
+				i++;
+			(str[i] == '/') ? (rest = ft_strdup(&(str[i]))) : (rest = ft_strdup(""));
+			tilde_prefix = ft_strsub(str, 1, i - 1);
+			if (!ft_strcmp(tilde_prefix, ""))
+			{// $HOME
+				tmp = fetch_variables("HOME", -1);
+				if (tmp)
+				{
+					ft_strdel(&str);
+					str = ft_strjoin(tmp, rest);
+				}
+				else
+				{
+					pw = getpwuid(getuid());
+					ft_strdel(&str);
+					if (pw)
+						str = ft_strjoin(pw->pw_dir, rest);
+				}
+			}
+			else if(!ft_strcmp(tilde_prefix, "-"))
+			{// $OLDPWD | ~-
+				tmp = fetch_variables("OLDPWD", -1);
+				if(tmp)
+				{
+					ft_strdel(&str);
+					str = ft_strjoin(tmp, rest);
+				}
+			}
+			else if (!ft_strcmp(tilde_prefix, "+"))
+			{// $PWD
+				tmp = fetch_variables("PWD", -1);
+				if(tmp)
+				{
+					ft_strdel(&str);
+					str = ft_strjoin(tmp, rest);
+				}
+			}
+			else
+			{
+				tmp = ft_strjoin("/Users/", tilde_prefix);
+				if (!access(tmp, F_OK))
+				{
+					ft_strdel(&str);
+					str = ft_strjoin(tmp, rest);
+				}
+			}
+			ft_strdel(&rest);
+			ft_strdel(&tilde_prefix);
+			if (tmp)
+				ft_strdel(&tmp);
+			if ((i = is_assword(node->data)))
+			{
+				node->data[i] = '\0';
+				tmp = ft_strjoin(node->data, str);
+				ft_strdel(&(node->data));
+				ft_strdel(&str);
+				node->data = tmp;
+			}
+			else
+			{
+				ft_strdel(&(node->data));
+				node->data = str;
+			}
+		}
+		node = node->next;
+	}
+	return (0);
+}
--- lexer.c
+++ lexer.c
@@ -233,14 +233,74 @@ int		is_assword(char *str)
 	return (0);
 }
 
+char	*tilde_sub_var(char *rest, char *tilde_prefix)
+{
+	char			*tmp;
+	char			*s;
+	struct passwd	*pw;
+
+	tmp = NULL;
+	if (!ft_strcmp(tilde_prefix, ""))
+		tmp = fetch_variables("HOME", -1);
+	else if(!ft_strcmp(tilde_prefix, "-"))
+		tmp = fetch_variables("OLDPWD", -1);
+	else if (!ft_strcmp(tilde_prefix, "+"))
+		tmp = fetch_variables("PWD", -1);
+	s = NULL;
+	if (tmp)
+	{
+		s = ft_strjoin(tmp, rest);
+	}
+	else if (!ft_strcmp(tilde_prefix, ""))
+	{
+		pw = getpwuid(getuid());
+		if (pw)
+		{
+			s = ft_strjoin(pw->pw_dir, rest);
+		}
+	}
+	else
+	{
+		tmp = ft_strjoin("/Users/", tilde_prefix);
+		if (!access(tmp, F_OK))
+		{
+			s = ft_strjoin(tmp, rest);
+		}
+	}
+	ft_strdel(&tmp);
+	return (s);
+}
+
+
+
+char		*tilde_sub_2(char *str)
+{
+	char			*s;
+	char			*tilde_prefix;
+	char 			*rest;
+	int				i;
+
+	s = NULL;
+	i = 1;
+	while (str[i] && str[i] != '/')
+		i++;
+	(str[i] == '/') ? (rest = ft_strdup(&(str[i]))) : (rest = ft_strdup(""));
+	tilde_prefix = ft_strsub(str, 1, i - 1);
+	s = tilde_sub_var(rest, tilde_prefix);
+	if (s)
+		ft_strdel(&str);
+	else
+		s = str;
+	ft_strdel(&tilde_prefix);
+	ft_strdel(&rest);
+	return (s);
+}
+
 int		tilde_sub(t_list_token **cmd_token)// need to be normed and leak free
 {// neeed to add ass_word
 	char 			*tmp = NULL;
 	char			*str;
-	char 			*tilde_prefix;
-	char 			*rest;
 	t_list_token	*node;
-	struct passwd	*pw;
 	int				i;
 
 	node = *cmd_token;
@@ -257,61 +317,11 @@ int		tilde_sub(t_list_token **cmd_token)// need to be normed and leak free
 			str = ft_strdup(node->data);
 		if (str && str[0] == '~')
 		{
-			i = 1;
-			while (str[i] && str[i] != '/')
-				i++;
-			(str[i] == '/') ? (rest = ft_strdup(&(str[i]))) : (rest = ft_strdup(""));
-			tilde_prefix = ft_strsub(str, 1, i - 1);
-			if (!ft_strcmp(tilde_prefix, ""))
-			{// $HOME
-				tmp = fetch_variables("HOME", -1);
-				if (tmp)
-				{
-					ft_strdel(&str);
-					str = ft_strjoin(tmp, rest);
-				}
-				else
-				{
-					pw = getpwuid(getuid());
-					ft_strdel(&str);
-					if (pw)
-						str = ft_strjoin(pw->pw_dir, rest);
-				}
-			}
-			else if(!ft_strcmp(tilde_prefix, "-"))
-			{// $OLDPWD | ~-
-				tmp = fetch_variables("OLDPWD", -1);
-				if(tmp)
-				{
-					ft_strdel(&str);
-					str = ft_strjoin(tmp, rest);
-				}
-			}
-			else if (!ft_strcmp(tilde_prefix, "+"))
-			{// $PWD
-				tmp = fetch_variables("PWD", -1);
-				if(tmp)
-				{
-					ft_strdel(&str);
-					str = ft_strjoin(tmp, rest);
-				}
-			}
-			else
-			{
-				tmp = ft_strjoin("/Users/", tilde_prefix);
-				if (!access(tmp, F_OK))
-				{
-					ft_strdel(&str);
-					str = ft_strjoin(tmp, rest);
-				}
-			}
-			ft_strdel(&rest);
-			ft_strdel(&tilde_prefix);
-			if (tmp)
-				ft_strdel(&tmp);
+			str = tilde_sub_2(str);
+			// tilde_sub_in_token()
 			if ((i = is_assword(node->data)))
 			{
-				node->data[i] = '\0';
+				node->data[i] = '\0';// !! free problem ?
 				tmp = ft_strjoin(node->data, str);
 				ft_strdel(&(node->data));
 				ft_strdel(&str);
@@ -320,9 +330,10 @@ int		tilde_sub(t_list_token **cmd_token)// need to be normed and leak free
 			else
 			{
 				ft_strdel(&(node->data));
-				node->data = str;
+				node->data = ft_strdup(str);
 			}
 		}
+		ft_strdel(&str);
 		node = node->next;
 	}
 	return (0);
